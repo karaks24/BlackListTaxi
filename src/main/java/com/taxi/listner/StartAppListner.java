3464a865dd3cf6be361a45000594bbd6189dfbbc
Olexandr Berkovskyy <olexandr_berkovskyy@epam.com>
2016-03-16 16:44:20 +0200

    asfd

--- src/main/java/com/taxi/listner/StartAppListner.java
+++ src/main/java/com/taxi/listner/StartAppListner.java
@@ -1,18 +1,38 @@
 package com.taxi.listner;
 
+import com.google.common.reflect.ClassPath;
 import com.sun.jersey.guice.JerseyServletModule;
 import com.sun.jersey.guice.spi.container.servlet.GuiceContainer;
-import com.taxi.MyResource;
 import com.taxi.service.Service;
 import com.taxi.service.ServiceImpl;
+
+import java.io.IOException;
 
 
 public class StartAppListner extends JerseyServletModule {
 
+    private final String CONTROLLERS_PACKAGE = "com.taxi.controllers";
+
+    private void controllerBinder(){
+        ClassLoader contextClassLoader = Thread.currentThread()
+                .getContextClassLoader();
+        ClassPath classPath = null;
+        try {
+            classPath = ClassPath.from(contextClassLoader);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        for (ClassPath.ClassInfo info : classPath.getTopLevelClassesRecursive(CONTROLLERS_PACKAGE)){
+            bind(info.load()).asEagerSingleton();
+        }
+
+    }
+
     @Override
     protected void configureServlets() {
-        bind(MyResource.class);
+        controllerBinder();
         bind(Service.class).to(ServiceImpl.class);
         serve("/*").with(GuiceContainer.class);
     }
+
 }
